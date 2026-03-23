275dddddffff94864fac25e5ec7feb8e05dc0fae
hiko667 <91916@student.pb.edu.pl>
2026-03-23 15:14:07 +0100

    fixed name

---
+++ Models/PersonModel.cs
@@ -0,0 +1,9 @@
+namespace Person.Models;
+
+public class Person
+{
+    public int Id {get; set;}
+    public string Name {get; set;}
+    public string LastName {get; set;}
+    public bool IsSecretlyAHamster {get; set;}
+}
