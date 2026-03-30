f16bc4fe376a8ebdc44d9be0a52b474133fb50cd
hiko667 <91916@student.pb.edu.pl>
2026-03-30 15:56:14 +0200

    Fixed connection of SpottedAts

--- Data/PersonContext.cs
+++ Data/PersonContext.cs
@@ -6,4 +6,9 @@ public class PersonContext : DbContext
     public PersonContext(DbContextOptions options) : base(options){}
     public DbSet<ConcretePerson> ConcretePerson {get; set;}
     public DbSet<Group> Group {get; set;}
+    public DbSet<SpottedAt> SpottedAts {get; set;}
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ConcretePerson>().HasMany(e => e.spottedAt).WithMany(e => e.People);
+    }
 }
--- obj/Debug/net9.0/Person.AssemblyInfo.cs
+++ obj/Debug/net9.0/Person.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Person")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+280c38b28aa78e2aa7388af81595468f49a1f244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3858b4ce71784d2b1b9f4f1f49c21d7f91afbda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Person")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Person")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
